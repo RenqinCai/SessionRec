91ba56802339c9e2a9d46e739b4b827eab2eacc8
rc7ne <rc7ne@hcdmg2.cs.virginia.edu>
2019-07-03 17:32:12 -0400

    add negative sample based category mixture attention model

---
+++ sampleCategoryMixtureAtt/main_time.py
@@ -0,0 +1,253 @@
+"""
+use time threshold to cut sequences
+command 
+CUDA_VISIBLE_DEVICES=1 python main.py --data_folder ../Data/tmall/100000_unknown_cate/ --data_action item.pickle --data_cate cate.pickle --data_time time.pickle --data_name taobao --embedding_dim 300 --hidden_size 300 --lr 0.0001 --window_size 5 --test_observed 5 --n_epochs 300 --shared_embedding 1 --batch_size 200 --optimizer_type Adam 
+"""
+
+import argparse
+import torch
+# import lib
+import numpy as np
+import os
+import datetime
+from dataset_time_cut import *
+# from dataset import *
+from loss import *
+from model import *
+from optimizer import *
+from trainer import *
+from torch.utils import data
+import pickle
+import sys
+import logger
+
+import sys
+sys.path.insert(0, '../PyTorch_GBW_LM')
+sys.path.insert(0, '../PyTorch_GBW_LM/log_uniform')
+
+from sparse_model import RNNModel, SampledSoftmax
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--hidden_size', default=50, type=int)
+parser.add_argument('--num_layers', default=1, type=int)
+parser.add_argument('--batch_size', default=100, type=int)
+parser.add_argument('--dropout_input', default=0, type=float)
+parser.add_argument('--dropout_hidden', default=.2, type=float)
+
+# parse the optimizer arguments
+parser.add_argument('--optimizer_type', default='Adagrad', type=str)
+parser.add_argument('--final_act', default='tanh', type=str)
+parser.add_argument('--lr', default=.05, type=float)
+parser.add_argument('--weight_decay', default=0, type=float)
+parser.add_argument('--momentum', default=0.1, type=float)
+parser.add_argument('--eps', default=1e-6, type=float)
+
+parser.add_argument("-seed", type=int, default=7,
+					 help="Seed for random initialization")
+parser.add_argument("-sigma", type=float, default=None,
+					 help="init weight -1: range [-sigma, sigma], -2: range [0, sigma]")
+parser.add_argument("--embedding_dim", type=int, default=-1,
+					 help="using embedding")
+# parse the loss type
+parser.add_argument('--loss_type', default='TOP1', type=str)
+# parser.add_argument('--loss_type', default='BPR', type=str)
+parser.add_argument('--topk', default=5, type=int)
+# etc
+parser.add_argument('--bptt', default=1, type=int)
+parser.add_argument('--test_observed', default=5, type=int)
+parser.add_argument('--window_size', default=30, type=int)
+parser.add_argument('--warm_start', default=5, type=int)
+
+parser.add_argument('--n_epochs', default=20, type=int)
+parser.add_argument('--time_sort', default=False, type=bool)
+parser.add_argument('--model_name', default='GRU4REC', type=str)
+parser.add_argument('--save_dir', default='models', type=str)
+parser.add_argument('--data_folder', default='../Data/movielen/1m/', type=str)
+parser.add_argument('--data_action', default='item.pickle', type=str)
+parser.add_argument('--data_cate', default='cate.pickle', type=str)
+parser.add_argument('--data_time', default='time.pickle', type=str)
+# parser.add_argument('--valid_data', default='test_item.pickle', type=str)
+# parser.add_argument('--test_data', default='test_item.pickle', type=str)
+parser.add_argument("--is_eval", action='store_true')
+parser.add_argument('--load_model', default=None,  type=str)
+parser.add_argument('--checkpoint_dir', type=str, default='checkpoint')
+parser.add_argument('--data_name', default=None, type=str)
+parser.add_argument('--shared_embedding', default=None, type=int)
+parser.add_argument('--patience', default=1000, type=int)
+parser.add_argument('--negative_num', default=1000, type=int)
+
+# Get the arguments
+args = parser.parse_args()
+args.cuda = torch.cuda.is_available()
+
+np.random.seed(args.seed)
+torch.manual_seed(7)
+
+if args.cuda:
+	torch.cuda.manual_seed(args.seed)
+
+def make_checkpoint_dir():
+	print("PARAMETER" + "-"*10)
+	now = datetime.datetime.now()
+	S = '{:02d}{:02d}{:02d}{:02d}'.format(now.month, now.day, now.hour, now.minute)
+	save_dir = os.path.join(args.checkpoint_dir, S)
+	if not os.path.exists(args.checkpoint_dir):
+		os.mkdir(args.checkpoint_dir)
+
+	if not os.path.exists(save_dir):
+		os.mkdir(save_dir)
+
+	args.checkpoint_dir = save_dir
+
+	with open(os.path.join(args.checkpoint_dir, 'parameter.txt'), 'w') as f:
+		for attr, value in sorted(args.__dict__.items()):
+			print("{}={}".format(attr.upper(), value))
+			f.write("{}={}\n".format(attr.upper(), value))
+
+	print("---------" + "-"*10)
+
+def init_model(model):
+	if args.sigma is not None:
+		for p in model.parameters():
+			if args.sigma != -1 and args.sigma != -2:
+				sigma = args.sigma
+				p.data.uniform_(-sigma, sigma)
+			elif len(list(p.size())) > 1:
+				sigma = np.sqrt(6.0 / (p.size(0) + p.size(1)))
+				if args.sigma == -1:
+					p.data.uniform_(-sigma, sigma)
+				else:
+					p.data.uniform_(0, sigma)
+
+def count_parameters(model):
+	parameter_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
+	print("parameter_num", parameter_num) 
+
+def main():
+
+	# ntokens = 1000
+	# nsampled = 100
+
+	# ss = SampledSoftmax(ntokens, nsampled, 200, tied_weight=None)
+	# print(ss.params.weight.data)
+
+	# exit()
+	
+	hidden_size = args.hidden_size
+	num_layers = args.num_layers
+	batch_size = args.batch_size
+	dropout_input = args.dropout_input
+	dropout_hidden = args.dropout_hidden
+	embedding_dim = args.embedding_dim
+	final_act = args.final_act
+	loss_type = args.loss_type
+	topk = args.topk
+	optimizer_type = args.optimizer_type
+	lr = args.lr
+	weight_decay = args.weight_decay
+	momentum = args.momentum
+	eps = args.eps
+	BPTT = args.bptt
+
+	n_epochs = args.n_epochs
+	time_sort = args.time_sort
+
+	window_size = args.window_size
+
+	log = logger.Logger()
+	log.addIOWriter(args)
+
+	shared_embedding = args.shared_embedding
+	message = "main_time.py shared_embedding: "+str(shared_embedding)
+	log.addOutput2IO(message)
+
+	if embedding_dim == -1:
+		message = "embedding dim not -1 "+str(embedding_dim)
+		log.addOutput2IO(message)
+		raise AssertionError()
+
+	message = "*"*10
+	log.addOutput2IO(message)
+	message = "train load, valid, test load"
+	log.addOutput2IO(message)
+
+	data_action = args.data_folder+args.data_action
+	data_cate = args.data_folder+args.data_cate
+	data_time = args.data_folder+args.data_time
+
+	observed_threshold = args.test_observed
+	data_obj = Data(data_action, data_cate, data_time, observed_threshold, window_size)
+	train_data = data_obj.train_dataset
+	valid_data = data_obj.test_dataset
+	test_data = data_obj.test_dataset
+	
+	input_size = len(data_obj.items)
+	output_size = input_size
+
+	negative_num = args.negative_num
+
+	message = "input_size "+str(input_size)
+	log.addOutput2IO(message)
+
+	train_data_loader = dataset.DataLoader(train_data, batch_size, negative_num)
+	valid_data_loader = dataset.DataLoader(valid_data, batch_size, negative_num)
+
+	if not args.is_eval:
+		make_checkpoint_dir()
+
+	if not args.is_eval:
+		model = GRU4REC(log, window_size, input_size, hidden_size, output_size,
+							final_act=final_act,
+							num_layers=num_layers,
+							use_cuda=args.cuda,
+							batch_size=batch_size,
+							dropout_input=dropout_input,
+							dropout_hidden=dropout_hidden,
+							embedding_dim=embedding_dim, 
+							shared_embedding=shared_embedding
+							)
+		# model = model.to(used_cuda_device)
+
+		# init weight
+		# See Balazs Hihasi(ICLR 2016), pg.7
+		
+		count_parameters(model)
+
+		init_model(model)
+
+		optimizer = Optimizer(model.parameters(),
+								  optimizer_type=optimizer_type,
+								  lr=lr,
+								  weight_decay=weight_decay,
+								  momentum=momentum,
+								  eps=eps)
+
+		loss_function = LossFunction(loss_type=loss_type, use_cuda=args.cuda)
+
+		trainer = Trainer(log, model,
+							  train_data=train_data_loader,
+							  eval_data=valid_data_loader,
+							  optim=optimizer,
+							  use_cuda=args.cuda,
+							  loss_func=loss_function,
+							  topk = args.topk,
+							  args=args)
+
+		trainer.train(0, n_epochs - 1, batch_size)
+	else:
+		if args.load_model is not None:
+			print("Loading pre trained model from {}".format(args.load_model))
+			checkpoint = torch.load(args.load_model)
+			model = checkpoint["model"]
+			model.gru.flatten_parameters()
+			optim = checkpoint["optim"]
+			loss_function = LossFunction(loss_type=loss_type, use_cuda=args.cuda)
+			evaluation = Evaluation(model, loss_function, use_cuda=args.cuda)
+			loss, recall, mrr = evaluation.eval(valid_data)
+			print("Final result: recall = {:.2f}, mrr = {:.2f}".format(recall, mrr))
+		else:
+			print("Pre trained model is None!")
+
+
+if __name__ == '__main__':
+	main()
---
+++ sampleCategoryMixtureAtt/model.py
@@ -0,0 +1,143 @@
+from torch import nn
+import torch
+import torch.nn.functional as F
+import numpy as np
+from torch.nn.utils.rnn import pad_sequence
+
+class GRU4REC(nn.Module):
+    def __init__(self, log, window_size, input_size, hidden_size, output_size, num_layers=1, final_act='tanh', dropout_hidden=.8, dropout_input=0, batch_size=50, embedding_dim=-1, use_cuda=False, shared_embedding=True):
+        super(GRU4REC, self).__init__()
+        self.m_log = log
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.num_layers = num_layers
+        self.dropout_hidden = dropout_hidden
+        self.dropout_input = dropout_input
+        self.embedding_dim = embedding_dim
+        self.batch_size = batch_size
+        self.use_cuda = use_cuda
+        self.window_size = window_size
+        self.device = torch.device('cuda' if use_cuda else 'cpu')
+        
+        self.fc = nn.Linear(hidden_size*2, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+            
+        self.create_final_activation(final_act)
+
+        self.look_up = nn.Embedding(input_size, self.embedding_dim).to(self.device)
+        self.m_cate_gru = nn.GRU(self.embedding_dim, self.hidden_size, self.num_layers, dropout=self.dropout_hidden, batch_first=True)
+        self.m_short_gru = nn.GRU(self.embedding_dim, self.hidden_size, self.num_layers, dropout=self.dropout_hidden, batch_first=True)
+
+        if shared_embedding:
+           
+            message = "share embedding"
+            self.m_log.addOutput2IO(message)
+
+            self.h2o.weight.data = self.look_up.weight.data
+           
+        self = self.to(self.device)
+
+    def create_final_activation(self, final_act):
+        if final_act == 'tanh':
+            self.final_activation = nn.Tanh()
+        elif final_act == 'relu':
+            self.final_activation = nn.ReLU()
+        elif final_act == 'softmax':
+            self.final_activation = nn.Softmax()
+        elif final_act == 'softmax_logit':
+            self.final_activation = nn.LogSoftmax()
+        elif final_act.startswith('elu-'):
+            self.final_activation = nn.ELU(alpha=float(final_act.split('-')[1]))
+        elif final_act.startswith('leaky-'):
+            self.final_activation = nn.LeakyReLU(negative_slope=float(final_act.split('-')[1]))
+
+    def forward(self, input_cate_batch, mask_cate_batch, mask_cate_seq_batch, max_actionNum_cate_batch, max_subseqNum_cate_batch, subseqLen_cate_batch, seqLen_cate_batch, input_batch, mask_batch, seqLen_batch, train_test_flag):
+
+        embedded_cate = input_cate_batch
+        embedded_cate = self.look_up(embedded_cate)
+
+        batch_size_cate = embedded_cate.size(0)
+        hidden_subseq_cate = self.init_hidden(batch_size_cate)
+
+        ### embedded: batch_size*seq_len*hidden_size
+        output_subseq_cate, hidden_subseq_cate = self.m_cate_gru(embedded_cate, hidden_subseq_cate) # (sequence, B, H)
+
+        mask_cate_batch = mask_cate_batch.unsqueeze(-1)
+       
+        output_subseq_cate = output_subseq_cate*mask_cate_batch
+        
+        # pad_subseqLen_cate_batch = np.array([i-1 if i > 0 else 0 for i in subseqLen_cate_batch])
+        first_dim_index = torch.arange(batch_size_cate).to(self.device)
+        second_dim_index = torch.from_numpy(subseqLen_cate_batch).to(self.device)
+        
+        input_seq_cate = output_subseq_cate[first_dim_index, second_dim_index, :]
+
+        ### batch_size_seq*seq_len*hidden_size
+        input_seq_cate = input_seq_cate.reshape(-1, max_subseqNum_cate_batch, output_subseq_cate.size(-1))
+        input_seq_cate = input_seq_cate.contiguous()
+        
+        ### get the output from 5 latest actions
+        embedded_short = input_batch
+        embedded_short = self.look_up(embedded_short)
+
+        batch_size_short = embedded_short.size(0)
+        hidden_subseq_shrot = self.init_hidden(batch_size_short)
+
+        output_subseq_short, hidden_subseq_shrot = self.m_short_gru(embedded_short, hidden_subseq_shrot)
+
+        mask_short_batch = mask_batch.unsqueeze(-1).float()
+        output_subseq_short = output_subseq_short*mask_short_batch
+
+        # pad_seqLen_batch = [i-1 if i > 0 else 0 for i in seqLen_batch]
+        first_dim_index = torch.arange(batch_size_short).to(self.device)
+        second_dim_index = torch.from_numpy(seqLen_batch).to(self.device)
+
+        ### batch_size*hidden_size
+        output_short = output_subseq_short[first_dim_index, second_dim_index, :]
+        # output_seq, hidden_seq = self.m_short_gru(input_seq, hidden_seq)
+       
+        output_short = output_short.unsqueeze(-1)
+
+        # print("output_short", output_short.size())
+        # print("input_seq_cate size", input_seq_cate.size())
+        weight = torch.matmul(input_seq_cate, output_short)
+        weight = weight.squeeze(-1)
+        weight_normalized = F.softmax(weight, dim=1)
+
+        weight_normalized_mask = weight_normalized*mask_cate_seq_batch
+        weight_normalized_mask_sum = torch.sum(weight_normalized_mask, dim=1)
+        # print("weight size", weight_normalized_mask.size(), weight_normalized_mask_sum.size())
+        weight_normalized_mask_sum = weight_normalized_mask_sum.unsqueeze(-1)
+        weight_normalized_mask = weight_normalized_mask/weight_normalized_mask_sum
+
+        weight_normalized_mask = weight_normalized_mask.unsqueeze(-1)
+        weighted_input_seq_cate = weight_normalized_mask*input_seq_cate
+
+        sum_input_seq_cate = torch.sum(weighted_input_seq_cate, dim=1)
+        output_short = output_short.squeeze()
+
+        mixture_output = torch.cat((sum_input_seq_cate, output_short), dim=1)
+        fc_output = self.fc(mixture_output)
+        # mixture_output = sum_input_seq_cate+output_short
+
+        output = self.h2o(fc_output)
+
+        logit = output
+
+        return logit
+
+    def embedding_dropout(self, input):
+        p_drop = torch.Tensor(input.size(0), input.size(1), 1).fill_(1 - self.dropout_input)  # (B,1)
+        mask = torch.bernoulli(p_drop).expand_as(input) / (1 - self.dropout_input)  # (B,C)
+        mask = mask.to(self.device)
+        input = input * mask  # (B,C)
+
+        return input
+
+    def init_hidden(self, batch_size):
+        '''
+        Initialize the hidden state of the GRU
+        '''
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(self.device)
+        return h0
