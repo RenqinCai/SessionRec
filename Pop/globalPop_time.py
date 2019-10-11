4e9525fcc59dc733a3ea69294738042e9a4b5c81
rc7ne <rc7ne@hcdm2.cs.virginia.edu>
2019-10-10 20:51:27 -0400

    update files

---
+++ Pop/globalPop_time.py
@@ -0,0 +1,154 @@
+import pickle
+import random
+import numpy as np
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--topk', default=1, type=int)
+# etc
+parser.add_argument('--bptt', default=5, type=int)
+
+parser.add_argument('--data_folder', default='../Data/movielen/1m/', type=str)
+parser.add_argument('--data_action', default='item.pickle', type=str)
+parser.add_argument('--data_cate', default='cate.pickle', type=str)
+parser.add_argument('--data_time', default='time.pickle', type=str)
+
+parser.add_argument("--is_eval", action='store_true')
+parser.add_argument('--checkpoint_dir', type=str, default='checkpoint')
+
+parser.add_argument('--valid_start_time', default=0, type=int)
+parser.add_argument('--test_start_time', default=0, type=int)
+
+# Get the arguments
+args = parser.parse_args()
+
+data_action = args.data_folder+args.data_action
+data_cate = args.data_folder+args.data_cate
+data_time = args.data_folder+args.data_time
+
+action_f = open(data_action, "rb")
+action_seq_arr_total = pickle.load(action_f)
+
+cate_f = open(data_cate, "rb")
+cate_seq_arr_total = pickle.load(cate_f)
+
+time_f = open(data_time, "rb")
+time_seq_arr_totoal = pickle.load(time_f)
+
+action_seq_num = len(action_seq_arr_total)
+print("action seq num", action_seq_num)
+
+cate_seq_num = len(cate_seq_arr_total)
+print("cate seq num", cate_seq_num)
+
+time_seq_num = len(time_seq_arr_totoal)
+print("time seq num", time_seq_num)
+
+valid_start_time = args.valid_start_time
+print("valid_start_time", valid_start_time)
+
+test_start_time = args.test_start_time
+print("test start time", test_start_time)
+
+train_total_action_list = []
+test_total_action_list = []
+
+for seq_index in range(action_seq_num):
+	action_seq_arr = action_seq_arr_total[seq_index]
+	time_seq_arr = time_seq_arr_totoal[seq_index]
+
+	actionNum_seq = len(action_seq_arr)
+
+	valid_start_action_index = -1
+	test_start_action_index = -1
+
+	for action_index in range(actionNum_seq):
+		item_cur = action_seq_arr[action_index]
+		time_cur = time_seq_arr[action_index]
+
+		if time_cur > valid_start_time:
+			if valid_start_action_index == -1:
+				valid_start_action_index = action_index
+
+		if time_cur > test_start_time:
+			if test_start_action_index == -1:
+				test_start_action_index = action_index
+				break
+	
+	subseq_train = action_seq_arr[:valid_start_action_index]
+	train_total_action_list.append(subseq_train)
+
+	subseq_test = action_seq_arr[valid_start_action_index: test_start_action_index]
+	test_total_action_list.append(subseq_test)
+
+seq_action_num_threshold = 5
+
+topk = 5
+
+correct_num = 0
+total_num = 0
+
+print("*"*10, "global pop", "*"*10)
+
+train_user_num = len(train_total_action_list)
+item_freq_map = {}
+
+item_id_map = {}
+id_item_map = {}
+
+for train_index in range(train_user_num):
+	train_action_user_list = train_total_action_list[train_index]
+	train_action_user_num = len(train_action_user_list)
+	
+	for train_action_user_index in range(train_action_user_num):
+		train_user_item = train_action_user_list[train_action_user_index]
+		
+		if train_user_item not in item_freq_map:
+			item_freq_map[train_user_item] = 0.0
+
+		if train_user_item not in item_id_map:
+			item_id = len(item_id_map)
+			item_id_map[train_user_item] = item_id
+			id_item_map[item_id] = train_user_item
+			
+		item_freq_map[train_user_item] += 1.0
+
+
+topk = args.topk
+
+correct_num = 0
+total_num = 0
+mrr_num = 0
+
+sorted_item_list = sorted(item_freq_map, key=item_freq_map.__getitem__, reverse=True)
+print("top k items", sorted_item_list[:topk])
+
+for i in sorted_item_list[:topk]:
+	print(item_id_map[i], item_freq_map[i])
+
+test_user_num = len(test_total_action_list)
+for test_index in range(test_user_num):
+	test_action_user_list = test_total_action_list[test_index]
+	test_action_user_num = len(test_action_user_list)
+	
+	for test_action_user_index in range(test_action_user_num):
+		test_mrr = 0.0
+		test_user_item = test_action_user_list[test_action_user_index]
+		topk_candidate_item_list = sorted_item_list[:topk]
+
+		if test_user_item in topk_candidate_item_list:
+			correct_num += 1.0
+			test_rank = list(topk_candidate_item_list).index(test_user_item)
+			test_mrr = 1.0/(test_rank+1.0)
+
+		total_num += 1.0
+		mrr_num += test_mrr
+
+print("correct num", correct_num)
+print("total num", total_num)
+print("hit@{0:d}: {1:.4f}".format(topk, correct_num/total_num))
+print("mrr@{0:d}: {1:.4f}".format(topk, mrr_num/total_num))
+
+		
