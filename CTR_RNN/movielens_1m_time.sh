c4a74ebdec7863f8f646fdeeff24f31e05da346c
rc7ne <rc7ne@hcdmg3.cs.virginia.edu>
2020-01-12 15:51:04 -0500

    add bash file for multipleGPU

---
+++ CTR_RNN/movielens_1m_time.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python main_time.py --data_folder ../Data/ctr/ --data_action movielen_1M_time --data_name movielens --embedding_dim 64 --hidden_size 64 --lr 0.002 --test_observed 5 --n_epochs 100 --shared_embedding 1 --batch_size 300 --optimizer_type Adam --loss_type 'XE' --negative_num 1000 --topk 20
